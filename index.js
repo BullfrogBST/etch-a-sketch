08c4ac3d92b0cc7515a881fbc7d69b44533834fe
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-16 15:58:25 -0500

    Update JS pseudocode, declare variables, check if tilesInput is valid

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-//Capture the grid container, input value, clear button, stylesheet root, and mouse position, and capture them in variables
+//Capture the grid container, input value, clear button, stylesheet root, and feature stats, and capture them in variables or objects
+const gridContainer = document.querySelector('.grid-container');
+const tilesInput = document.querySelector('.tile-input');
+const messageContainer = document.querySelector('.message-container');
 
-//Make a variable for how many tiles have been selected
+const featureStats = {
+    tilesStartValue: 64,
+    errorMessage: document.createElement('p')
+}
 
-//Make a constructor function for the tiles, and make each item a div with a class of grid-tile
+tilesInput.value = featureStats.tilesStartValue;
 
-//Make a new instance of the tile constructor function, with as many rows and columns as the input says. Make the default value for rows and columns 64
+//If the tilesInput value is blank or over 100, give an error message. Do this with the checkValidInput function
+
+function checkValidInput(){
+    return new Promise((resolve, reject) =>{
+        if(tilesInput.value > 100){
+            reject("Your input is too high! Please select a number between 1 and 100.")
+        } else if(tilesInput.value == ''){
+            reject("Please select a number of rows and columns");
+        } else if(tilesInput.value < 1){
+            reject("Your input is too low! Please select a number between 1 and 100")
+        } else{
+            resolve();
+        }
+    })
+}
+
+tilesInput.addEventListener('change', () =>{
+    checkValidInput()
+        .then(() =>{
+            featureStats.errorMessage.textContent = '';
+            console.log('Valid Input!');
+        })
+        .catch((err) =>{
+            featureStats.errorMessage.textContent = err;
+            messageContainer.appendChild(featureStats.errorMessage)
+        })
+})
+
+//Make a new div with the class grid-tile for as many columns and rows as the input value suggests. Make them a child of the grid container
+
 
 //For each grid-tile, check if it's been hovered over, and if so, call the mouseOverTile() function and pass the event target
 
