d8f1ad57f60bbfd7a66788ac99085b084f75c1d7
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-16 10:36:46 -0500

    Added JS pseudocode and clear button

---
+++ index.js
@@ -0,0 +1,21 @@
+//Capture the grid container, input value, clear button, stylesheet root, and mouse position, and capture them in variables
+
+//Make a variable for how many tiles have been selected
+
+//Make a constructor function for the tiles, and make each item a div with a class of grid-tile
+
+//Make a new instance of the tile constructor function, with as many rows and columns as the input says. Make the default value for rows and columns 64
+
+//For each grid-tile, check if it's been hovered over, and if so, call the mouseOverTile() function and pass the event target
+
+//Declare the mouseOverTile() function
+
+//Create a temporary variable for the color of the current tile, which is a random rgb value
+
+//Every time that specific tile is passed over, add 10% black to it
+
+//Add an event listener to the clear button, and call the clearGrid() function
+
+//Declare the clearGrid() function
+
+//For every tile on the grid, set the color back to white
